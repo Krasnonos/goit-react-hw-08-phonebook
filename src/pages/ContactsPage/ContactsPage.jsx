6a9d0f2701785bd8a5cd657f4e282bd1b1499271
Krasnonos <andreykrasnonos@icloud.com>
2022-08-26 21:16:21 +0300

    fix vars

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,18 +1,11 @@
-import { useDispatch } from 'react-redux';
-import { userLogout } from '../../redux/authOperation';
 import { ContactsList } from '../../components/ContactsList/ContactsList';
 import { PhoneBookForm } from '../../components/PhoneBookForm/PhoneBookForm';
 import { ContactsFilter } from '../../components/ContactsFilter/ContactsFilter';
-import { NavBtn, Tittle } from './ContactsPage.styled';
+import { Tittle } from './ContactsPage.styled';
 
 const ContactsPage = () => {
-  // const dispatch = useDispatch();
-
   return (
     <div>
-      {/* <NavBtn type="button" onClick={() => dispatch(userLogout())}>
-        Log Out
-      </NavBtn> */}
       <Tittle>Contacts</Tittle>
       <PhoneBookForm />
       <ContactsFilter />
--- src/pages/ContactsPage/ContactsPage.styled.js
+++ src/pages/ContactsPage/ContactsPage.styled.js
@@ -8,24 +8,3 @@ export const Tittle = styled.h1`
   font-size: ${p => p.theme.fontSizes.xl};
   line-height: 1.4;
 `;
-
-export const NavBtn = styled.button`
-  position: absolute;
-  top: 20px;
-  right: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 92px;
-  height: 42px;
-  margin-right: auto;
-  margin-left: auto;
-  border-radius: 3px;
-  border: ${p => p.theme.borders.none};
-  text-decoration: none;
-  text-transform: uppercase;
-  background-color: #3f51b5;
-  color: white;
-  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
-    0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
-`;
